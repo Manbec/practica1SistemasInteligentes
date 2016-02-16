30bc927eb5e6476d084cdee3afc9f420170376b9
Manbec <mbecerramarrufo@acm.org>
2016-02-16 16:37:39 -0600

    base depth recursivo

--- plano2D.js
+++ plano2D.js
@@ -1,21 +1,39 @@
 var map = [
-    ['X','X','X','X','X','X','X','X','X','X'],
-    ['X','_','_','_','X','X','_','X','_','X'],
-    ['X','_','X','_','_','X','_','_','_','X'],
-    ['X','S','X','X','_','_','_','X','_','X'],
-    ['X','_','X','_','_','X','_','_','_','X'],
-    ['X','_','_','_','X','X','_','X','_','X'],
-    ['X','_','X','_','_','X','_','X','_','X'],
-    ['X','_','X','X','_','_','_','X','_','X'],
-    ['X','_','_','O','_','X','_','_','_','X'],
-    ['X','X','X','X','X','X','X','X','X','X']   
+['X','X','X','X','X','X','X','X','X','X'],
+['X','_','_','_','X','X','_','X','_','X'],
+['X','_','X','_','_','X','_','_','_','X'],
+['X','_','X','X','_','_','_','X','_','X'],
+['X','_','X','_','_','X','_','_','_','X'],
+['X','_','_','_','X','X','_','X','_','X'],
+['X','_','X','_','_','X','_','X','_','X'],
+['X','_','X','X','_','_','_','X','S','X'],
+['X','_','_',0,'_','X','_','_','_','X'],
+['X','X','X','X','X','X','X','X','X','X']   
 ];
+
+Direction = {
+    UP: 0,
+    RIGHT: 1,
+    LEFT: 2,
+    DOWN: 3
+};
+
+var dirArray = [Direction.UP,Direction.RIGHT,Direction.LEFT,Direction.DOWN];
+var dirArrayStr = ["UP","RIGHT","LEFT","DOWN"];
 
 function Coord(x,y,weight){
     this.x = x;
     this.y = y;
     this.weight = weight;
 }
+
+function Coordirection(x,y,weight,direction){
+    this.x = x;
+    this.y = y;
+    this.weight = weight;
+    this.direction = direction;
+}
+
 
 var queue = [new Coord(3,8,0)];
 var path = [];
@@ -63,23 +81,27 @@ function checkAdjacents(coord){
     return adjacent;
 }
 
+function checkAdjacentsWithDirection(coord){
+    var adjacent = {};
+    adjacent[Direction.LEFT] = (new Coord(coord.x-1,coord.y,map[coord.y][coord.x-1]));
+    adjacent[Direction.RIGHT] = (new Coord(coord.x+1,coord.y,map[coord.y][coord.x+1]));
+    adjacent[Direction.UP] = (new Coord(coord.x,coord.y-1,map[coord.y-1][coord.x]));
+    adjacent[Direction.DOWN] = (new Coord(coord.x,coord.y+1,map[coord.y+1][coord.x]));
+    return adjacent;
+}
+
+
 function savePath(path){
-    var newNext=false;
-    for(var i=0; i<path.length;i++){
-        var adjacents=checkAdjacents(path[i]);
-        var next;
+    console.log("save path");
+    while(path[path.length-1].weight>0){
+        var adjacents=checkAdjacents(path[path.length-1]);
+        var next = adjacents[0];
         for(var j = 1; j<adjacents.length; j++){
-            if(adjacents[j].weight!="X"  && adjacents[j].weight!="_" && adjacents[j].weight<path[i].weight){
-                if(!next || adjacents[j].weight<next.weight){
-                    next=adjacents[j];
-                    newNext=true;
-                }
+            if((adjacents[j].weight!="X"  && adjacents[j].weight!="_" && adjacents[j].weight<next.weight) ||(adjacents[j].weight!="X"  && adjacents[j].weight!="_" && (next.weight=="X" || next.weight=="_"))) {
+                next=adjacents[j];
             }
         }
-        if(newNext){
-            path.push(next);
-            newNext=false;
-        }
+        path.push(next);
     }
 }
 
@@ -108,8 +130,8 @@ function searchPaths(queue){ //breadth search
                     }
                 }
             }
-            for(var j=0; j<adjacents.length; j++){
-                queue.push(adjacents[j]);
+            for(var h=0; h<adjacents.length; h++){
+                queue.push(adjacents[h]);
             }
         }
         else{
@@ -122,4 +144,139 @@ function searchPaths(queue){ //breadth search
     printPath(path);
 }
 
-searchPaths(queue);
+var road = [];
+
+function depthsearchPaths(queue,weight,direction){ //depth search
+    var found=false;
+    printMatrix(map);
+    var currentx =  queue[0].x;
+    var currenty =  queue[0].y;
+    var adjacents = checkAdjacentsWithDirection(queue[0]);
+    var next;
+    var hasAdjacents = false;
+    weight++;
+
+    for(var j = 0; j<4; j++){
+        console.log(dirArrayStr[direction] + " in " + (weight-1));
+        console.log(direction);
+        next = adjacents[direction];
+        //console.log(next);
+        if(next.weight=="_") {
+            map[next.y][next.x] = weight;
+            printMatrix(map);
+            sleep(500);
+            console.log(queue);
+            queue.splice(0,0,next);
+            var dir = direction + 0;
+            if (depthsearchPaths(queue,weight,dirArray[dir])){
+                road.push(next);
+                return true;
+            }
+            hasAdjacents  = true;
+        }
+        else if (next.weight=="S") {
+            console.log("found");
+            road.push(next);
+            return true;
+        }
+        direction = (direction+1)%4;
+    }
+    map[currenty][currentx] = '_';
+    console.log("backtrack");
+    printMatrix(map);
+    sleep(500);
+    return false;
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+  }
+}
+}
+
+//searchPaths(queue);
+//depthsearchPaths(queue,0, Direction.UP);
+//console.log(road);
+
+function distanceFrom(sourceCoord, destCoord){
+    Math.hypot = Math.hypot || function(x, y){ return Math.sqrt(x*x + y*y) }
+
+    var x = (destCoord.x - sourceCoord.x),
+    y = (destCoord.y - sourceCoord.y);
+
+    //console.log ("x: "+x+", y: "+y);
+    //console.log("distance from ("+sourceCoord.x+","+sourceCoord.y+")  to ("+destCoord.x+","+destCoord.y+") : " + Math.hypot(x, y));
+    return Math.hypot(x, y);
+}
+
+var goalLocation = new Coord(9,7,0);
+
+function greedySearchPaths(queue,weight){ //depth search
+    var found=false;
+    printMatrix(map);
+    //console.log(queue);
+    var nextState = (queue[0].x+","+queue[0].y);
+    var visitedStates = [nextState];
+    //console.log(visitedStates);
+    var current = queue[0];
+    var adjacents = checkAdjacents(queue[0]);
+    var next;
+    var hasAdjacents = false;
+    weight++;
+
+    while(queue.length>0 && !found){
+        //console.log(dirArrayStr[direction] + " in " + (weight-1));
+        //console.log(direction);
+        current = queue[0];
+        queue.splice(0,1);
+        nextState = (current.x+","+current.y);
+        visitedStates.push();
+        adjacents = checkAdjacents(current);
+
+        map[current.y][current.x] = weight;
+        console.log("queue");
+                console.log(queue);
+        var tempStates = [];
+        for(i = 0; i < adjacents.length; i++){
+            next = adjacents[i];
+            nextState = (next.x+","+next.y);
+            console.log(adjacents[i]);
+            if(next.weight=="_" && visitedStates.indexOf(nextState)==-1) {
+                printMatrix(map);
+                sleep(500);
+                if(distanceFrom(next, goalLocation)<distanceFrom(current, goalLocation)){
+                    console.log('better: '+ distanceFrom(next, goalLocation)+ ", than" + distanceFrom(current, goalLocation));
+                    console.log(next);
+                    console.log(",");
+                    tempStates.splice(0,0,next);
+                }
+                else{
+                    tempStates.push(next);
+                    console.log('worse');
+                }
+                hasAdjacents  = true;
+            }
+            else if (next.weight=="S") {
+                console.log("found");
+                console.log(next);
+                road.push(next);
+                return true;
+            }
+        }
+        while(tempStates.length>0){
+             queue.splice(0,0,tempStates.pop());
+        }
+        weight++;
+    }
+
+    map[current.y][current.x] = '_';
+    printMatrix(map);
+    sleep(500);
+    return false;
+}
+
+
+greedySearchPaths(queue,0);
